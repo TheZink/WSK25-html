21d73b79c7fb08a0d05fd4c611c87422021b2d56
TheZink <ilkka.sinkonen@gmail.com>
2025-04-11 11:04:26 +0300

    Updated leaflet map. Added functionality to display restaurants on the map with dynamic marker color change and popup button interaction

--- js/main.js
+++ js/main.js
@@ -10,9 +10,18 @@ const options = {
   };
 
 function success(pos) {
+
+    const blueIcon = L.divIcon({ className: 'blue-icon' }); // Restaurants location color
+    const greenIcon = L.divIcon({ className: 'green-icon' }); // Selected restaurant color
+    const redIcon = L.divIcon({ className: 'red-icon' }); // User location color
+
+    let selectedMarker = null; // To track the currently selected marker
+
+
     const crd = pos.coords;
   
-    const map = L.map('map').setView([60.192059, 24.945831], 13);
+    // Create a map
+    const map = L.map('map').setView([crd.latitude, crd.longitude], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
@@ -23,9 +32,27 @@ function success(pos) {
     .then((restaurants) => {
         restaurants.forEach(restaurant => {
             const coordinates = restaurant.location.coordinates;
+
+            // Create a marker with information
+            const restaurantLocation = L.marker([coordinates[1], coordinates[0]]).addTo(map)
+            .bindPopup(`<b>${restaurant.name}</b> 
+                <p>${restaurant.address}, ${restaurant.postalCode} ${restaurant.city}</p>
+                <button id="${restaurant._id}">Ruokalista</button>`,)
+            restaurantLocation.setIcon(blueIcon)
             
-            L.marker([coordinates[1], coordinates[0]]).addTo(map)
-            .bindPopup(`<b>${restaurant.name}</b> <p>${restaurant.address}, ${restaurant.city}</p>`,)
+            // Eventlistener to change selected marker's icon to green
+            restaurantLocation.on('click', () => {
+
+                // Reset previously selected marker
+                if (selectedMarker) { selectedMarker.setIcon(blueIcon);}
+
+                // Change selected marker color
+                restaurantLocation.setIcon(greenIcon);
+
+                // Update the selected marker
+                selectedMarker = restaurantLocation;
+                
+            })
         });
     })
     
@@ -33,9 +60,29 @@ function success(pos) {
         console.error('Error fetching restaurant data:', error);
     });
 
-    L.marker([crd.latitude, crd.longitude]).addTo(map)
-    .bindPopup('Olet täällä')
-    .openPopup();
+    // Add users location to the map
+    const userLocation = L.marker([crd.latitude, crd.longitude]).addTo(map)
+        .bindPopup('Olet täällä')
+    userLocation.setIcon(redIcon)
+
+
+    
+    // Eventlistener for popup button
+    map.on('popupopen', (e) => {
+        const popupButton = e.popup._contentNode.querySelector('button');
+
+        if (popupButton) {
+            popupButton.addEventListener('click', () => {
+                fetchData(urlMenu(popupButton.id))
+                .then((restaurant) => {
+                    console.log(restaurant.courses);
+                })
+            });
+        }
+    })
+
+
+
 }
   
   function error(err) {
