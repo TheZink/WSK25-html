d83974ac72de80a1659187e99b44797ccb551fad
TheZink <ilkka.sinkonen@gmail.com>
2025-04-11 15:59:04 +0300

    Edited url for login and user creation

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,30 +1,25 @@
 // Fetch all restaurants
 export const urlAllRestaurants = () => {
     return 'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'
-}
+};
 
 // Fetch restaurant by id
 export const urlRestaurantById = (id) => {
     return `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/${id}`
-}
+};
 
 // Fetch restaurant daily menu by id
 export const urlDailyMenu = (id) => {
     return `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${id}/fi`
-}
+};
 
 // Fetch user login
-export const urlLoginUser = (dataJson) => {
-     return {
-        url: 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login',
-        data: dataJson
-    };
+export const urlLoginUser = () => {
+     return 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login'
 }
-
-export const urlCreateUser = (dataJson) => {
-    return {
-        url: 'https://media2.edu.metropolia.fi/restaurant/api/v1/users',
-        data: dataJson
-    };
-
-}
+ 
+// Create user
+export const urlCreateUser = () => {
+    return 'https://media2.edu.metropolia.fi/restaurant/api/v1/users'
+     
+};
