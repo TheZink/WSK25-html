b19deb1a99fb045884ef718a2c797039ea5c136a
TheZink <ilkka.sinkonen@gmail.com>
2025-04-11 15:58:25 +0300

    Created another fetchData with url and options parameters

--- js/util.js
+++ js/util.js
@@ -1,7 +1,29 @@
 export const fetchData = async (url) => {
     try {
-
         const request = await fetch(`${url}`);
+
+        if (!request.ok){
+            throw new Error(`Fetch error:  ${request.status}`);
+        }
+
+        const data = await request.json();
+        return data;
+
+    } catch (error) {
+        console.error('error', error);
+
+    } finally {
+
+        console.log('Request complete');
+    }
+}
+
+export const fetchDataOptions = async (url, options) => {
+    try {
+        console.log(url)
+        console.log(options)
+
+        const request = await fetch(`${url}`, options);
 
         if (!request.ok){
             throw new Error(`Fetch error:  ${request.status}`);
