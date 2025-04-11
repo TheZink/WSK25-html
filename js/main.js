eb5aa7369cfb06bca24d2c192c4623dd592d5cbe
TheZink <ilkka.sinkonen@gmail.com>
2025-04-11 12:59:09 +0300

    Added functionality to display restaurant details and menu in a modal on popup button click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-import { urlRestaurants, urlMenu, urlUser } from "./baseUrl.js";
+import { urlAllRestaurants, urlRestaurantById, 
+    urlDailyMenu, urlLoginUser } from "./baseUrl.js";
 import { fetchData } from "./util.js";
+import { restaurantModal } from "./component.js";
+
+const searchModal = document.getElementById('restaurantModal');
 
 // leaflet map
 
@@ -28,7 +32,7 @@ function success(pos) {
 
   
     // Add restaurants to the map
-    fetchData(urlRestaurants())
+    fetchData(urlAllRestaurants())
     .then((restaurants) => {
         restaurants.forEach(restaurant => {
             const coordinates = restaurant.location.coordinates;
@@ -36,7 +40,9 @@ function success(pos) {
             // Create a marker with information
             const restaurantLocation = L.marker([coordinates[1], coordinates[0]]).addTo(map)
             .bindPopup(`<b>${restaurant.name}</b> 
-                <p>${restaurant.address}, ${restaurant.postalCode} ${restaurant.city}</p>
+                <p class="popup-p">Osoite: ${restaurant.address}, ${restaurant.postalCode} ${restaurant.city}</p>
+                <p class="popup-p">Puhelinnro: ${restaurant.phone}</p>
+                <p class="popup-p">Yhtiö: ${restaurant.company}</p>
                 <button id="${restaurant._id}">Ruokalista</button>`,)
             restaurantLocation.setIcon(blueIcon)
             
@@ -66,29 +72,43 @@ function success(pos) {
     userLocation.setIcon(redIcon)
 
 
-    
     // Eventlistener for popup button
     map.on('popupopen', (e) => {
         const popupButton = e.popup._contentNode.querySelector('button');
 
         if (popupButton) {
-            popupButton.addEventListener('click', () => {
-                fetchData(urlMenu(popupButton.id))
-                .then((restaurant) => {
-                    console.log(restaurant.courses);
-                })
+            popupButton.addEventListener('click', async () => {
+                const restData = await fetchData(urlRestaurantById(popupButton.id)) //Fetch selected restaurant information
+                const restMenu = await fetchData(urlDailyMenu(popupButton.id)) //Fetch selected restaurant daily menu
+
+                const { name, address, city, postalCode, phone, company } = restData;
+                const { courses } = restMenu;
+                
+                // Initialize modal-window
+                searchModal.innerHTML = "";
+                
+                const restModal= restaurantModal(name, address, city, postalCode, phone, company, courses);
+
+                searchModal.appendChild(restModal);
+                searchModal.style.display = "block";
+
+                const closeModal = document.querySelector(".close");
+
+                closeModal.addEventListener('click', () => {
+                    searchModal.style.display = "none";
+                });
+                
             });
         }
     })
-
-
+    
 
 }
-  
-  function error(err) {
+
+function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
+
+
   
-  navigator.geolocation.getCurrentPosition(success, error, options);
-
-
+navigator.geolocation.getCurrentPosition(success, error, options);
