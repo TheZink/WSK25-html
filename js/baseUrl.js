d9aaa52d868da567a3708b1c6c70724dfa412651
TheZink <ilkka.sinkonen@gmail.com>
2025-04-11 08:48:16 +0300

    Added toString() method to id and password

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -8,8 +8,8 @@ export const urlMenu = (id) => {
 
 export const urlUser = (id, password) => {
     const dataJson = {
-        "username": id,
-        "password": password
+        "username": id.toString(),
+        "password": password.toString()
     }
     return {
         url: 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login',
