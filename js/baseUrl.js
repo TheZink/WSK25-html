9014e285f134ecdc7a82f85e21240c46b0e5d13d
TheZink <ilkka.sinkonen@gmail.com>
2025-04-13 17:08:53 +0300

    Added new url to fetch user data and restaurant weekly menu

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -13,6 +13,11 @@ export const urlDailyMenu = (id) => {
     return `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${id}/fi`
 };
 
+// Fetch restaurant weekly menu by id
+export const urlWeeklyMenu = (id) => {
+    return `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/weekly/${id}/:fi`
+}
+
 // Fetch user login
 export const urlLoginUser = () => {
      return 'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login'
@@ -22,3 +27,8 @@ export const urlLoginUser = () => {
 export const urlCreateUser = () => {
     return 'https://media2.edu.metropolia.fi/restaurant/api/v1/users'
 };
+
+// Get user data
+export const urlGetUserData = () => {
+    return 'https://media2.edu.metropolia.fi/restaurant/api/v1/users/token'
+}
