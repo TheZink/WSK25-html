497a41fb9564953deff3b6349c845b75194fe25d
TheZink <ilkka.sinkonen@gmail.com>
2025-04-14 17:43:01 +0300

    Added functionality to display user profile data, including favorite restaurant and avatar

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,20 @@
-export const getUserData = () => {
+import { urlRestaurantById } from '../js/baseUrl.js';
+import { fetchData } from './util.js';
+
+export const showUserData = (data, element) => {
+    let restaurant = "";
+    if(data.favouriteRestaurant) {
+        restaurant = fetchData(urlRestaurantById(data.favouriteRestaurant))
+    }
+
+    element.innerHTML = `<h1 style="margin-top: 10px; margin-bottom: 30px; font-size: 50px;">Profiili</h1>
+                    <p>Käyttäjänimi: ${data.username}</p>
+                    <p>Sähköposti: ${data.email}</P>
+                    <p>Suosikki ravintola ${restaurant.name || 'Ei ole määritetty'}
+                    <img src=${data.avatar || ''}; style="margin-top: 10px;>
+                    `
+                    
+
 };
 
 export const postUserData = () => {
