112c5f42f156ebfad04c36edc945067882861037
TheZink <ilkka.sinkonen@gmail.com>
2025-04-17 13:43:02 +0300

    Refactor user authentication and profile update functionality; add user data fetching and deletion

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { urlAllRestaurants, urlRestaurantById,
     urlDailyMenu} from "./baseUrl.js";
 import { initializeMap } from "./map.js";
 import { restaurantModal } from "./component.js";
-import { logUserIn, createUser } from "./auth.js";
+import { logUserIn, putUser, postUser, getUser, deleteUser} from "./auth.js";
 import { postUserData, showUserData } from "./profile.js";
 
 // Search.html element
@@ -22,14 +22,11 @@ const createEmail = document.getElementById('createEmail');
 
 // Profile.html element
 const profileContainer = document.getElementById('profile');
-const profileUpdate = document.getElementById('profileUpdate')
+const profileUpdate = document.getElementById('profileUpdate');
 
-
-
-let userData = null;
-
-const storedUserData = JSON.parse(sessionStorage.getItem('userData'));
-const favouriteRestData = JSON.parse(sessionStorage.getItem('favouriteRestData'));
+let storedUserData = JSON.parse(sessionStorage.getItem('userData'));
+let storedUserToken = JSON.parse(sessionStorage.getItem('userToken'));
+let favouriteRestData = JSON.parse(sessionStorage.getItem('favouriteRestData'));
 
 // leaflet map
 // Statement check, if "mapContainer" exist before initializing
@@ -45,7 +42,6 @@ mapContainer ? (() => {
     function success(pos) {
 
         const userCoords = pos.coords;
-        console.log(userData);
 
         // Initialize the map with user location and restaurant data
         initializeMap(
@@ -71,18 +67,22 @@ loginForm ? (() => {
         event.preventDefault();
         
         const userName = loginName.value;
-        const password = loginPass.value;
+        const passWord = loginPass.value;
         
-        const fetchUser = await logUserIn(userName, password);
+        const fetchUser = await logUserIn(userName, passWord); // Fetch user login
         
-        // If login is succesfull, store login data and redirect user to the profile
         if (fetchUser) {
-            sessionStorage.setItem('userData', JSON.stringify(fetchUser));
+            const getData = await getUser(fetchUser.token); // Fetch users data
+
+            sessionStorage.setItem('userToken', JSON.stringify(fetchUser.token)) // Store user token
+            sessionStorage.setItem('userData', JSON.stringify(getData)); // Store user data
+
             window.location.href = 'profile.html';
-            console.log('Login succesfull:', fetchUser);
+
         } else {
+            alert("Väärä käyttäjätunnus tai salasana");
             console.log("Login failed")
-        }    
+        };    
     });
 })() 
 : null;
@@ -99,23 +99,26 @@ createForm ? (() => {
         const password = createPass.value;
         const email = createEmail.value;
         
-        const postUser = await createUser(username,password,email);
+        const createUser = await postUser(username,password,email);
         
-        if (postUser) {
-            sessionStorage.setItem('userData', JSON.stringify(postUser));
-            console.log('Register succesfull: ', postUser);
+        // If registration is successful, redirect user to login.html
+        if (createUser) {
+            alert("Rekisteröityminen onnistui! Nyt voit kirjautua sisään!")
+            window.location.href = '/login.html';
         } else {
+            alert("Rekisteröityminen epäonnistui. Syynä voi olla, että käyttäjänimi on varattu tai palvelussa on katko. Käytä toista käyttäjätunnusta tai yritä myöhemmin uudelleen.")
             console.log("Register failed")
         }
     })
 })() 
 : null;
 
-//Profile
+// Profile
 // Check if "profileContainer" exist before executing and adding event listeners
-profileContainer ? (() => {
 
-    showUserData(storedUserData.data, profileContainer);
+profileContainer ? (async () => {
+    showUserData(storedUserData, profileContainer);
+    
     const updateButton = document.querySelector('#updateB');
     const deleteButton = document.querySelector('#deleteB');
 
@@ -128,25 +131,79 @@ profileContainer ? (() => {
 
     if (deleteButton) {
         deleteButton.addEventListener('click', () => {
-            console.log("Delete pressed")
+            const purgeUser = deleteUser(storedUserToken)
+            if (purgeUser){
+                sessionStorage.removeItem('userData')
+                sessionStorage.removeItem('userToken');
+                storedUserData = null;
+                storedUserToken= null;
+                alert('Käyttäjätunnukseksi poistettiin onnistuneesti')
+                window.location.href = '/home.html';
+            } else {
+                alert('Virhe!')
+            }
         })
     }
 })()
 :null;
 
+// profileUpdate.html
+// Check if "profileUpdate" exist before executing and adding event listeners
 profileUpdate ? (() => {
-    postUserData(storedUserData.data, profileUpdate);
-    //TODO: Eventlistener
+    postUserData(storedUserData, profileUpdate);
+    
+    const postButton = document.getElementById('postButton');
+    const abortButton = document.getElementById('abortButton');
+    const userName = document.getElementById('userName');
+    const userPassword = document.getElementById('userPassword')
+    const userEmail = document.getElementById('userEmail');
+    const userAvatar = document.getElementById('userAvatar');
+
+    let dataStore = {};
+
+    postButton.addEventListener('click', async () => {
+        userName.value ? dataStore.username = userName.value: null;
+        userPassword.value ? dataStore.password = userPassword.value: null;
+        userEmail.value ? dataStore.email = userEmail.value : null;
+        userAvatar.value ? dataStore.avatar = userAvatar.value : null;
+
+        // Check if dataStore has any properties
+        if (Object.keys(dataStore).length > 0) {
+
+            const putResult = await putUser(storedUserToken, dataStore);
+
+            if (putResult) {
+
+                const getSession = await getUser(storedUserToken) //Fetch fresh userData
+                console.log('updateSession ', getSession)
+                sessionStorage.removeItem('userData')
+                sessionStorage.setItem('userData', JSON.stringify(getSession)); //Update sessionStorage
+
+                window.location.href = '/profile.html';
+                alert('Tiedot päivitettiin onnistuneesti!');
+
+            } else {
+                alert('Tietojen päivittäminen epäonnistui. Yritäthän myöhemmin uudelleen.')
+                
+            }
+        } else {
+            alert('Et ole tehnyt muutoksia. Jos haluat päivittää tietojasi, täytä kentät. Muussa tapauksessa valitse "Peruuta" palataksesi takaisin.');
+        }
+        
+
+    });
+
+    abortButton.addEventListener('click', (evt) => {
+        window.location.href = '/profile.html'
+        alert('Palataan takaisin profiilin sivulle. Tietoja ei muutettu.')
+    })
 
 })()
 :null;
 
-
 // Restore userData from sessionstorage if not null
-if (storedUserData != null) {
+if (storedUserData != null && storedUserToken != null) {
     console.log('Retrieved user data from sessionstorage:', storedUserData);
-    userData = storedUserData; 
-    console.log('userData is:', userData);
 
     // Add logout button and event listener, when user is logged in
     const headerElement = document.getElementById('header_a');
@@ -163,10 +220,7 @@ if (storedUserData != null) {
 
         headerElement.appendChild(logout);
     }
-
-} else {
-    console.log('No userdata in sessionstorage')
-};
+}
 
 // Redirect for unauthenticated users accessing profile.html
 if (window.location.href.includes('profile.html') && storedUserData == null){
